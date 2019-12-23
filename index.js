30311d6985b10c844306bd19d7a9b4d263012c7d
duyuqian <15738381799@163.com>
2019-12-23 20:15:05 +0800

    [duyuqian] feat: when click the selection box,the text turns gray and strikethrough appears

--- index.js
+++ index.js
@@ -1,10 +1,84 @@
 var addButton = document.getElementsByClassName('button-add')[0];
 var addInput = document.getElementsByClassName('input-add')[0];
 var toDoList = document.getElementsByTagName('ol')[0];
+var toDoThings = document.getElementsByTagName('li');
+// var choseButton = document.getElementsByClassName('button-chose');
+// var allButton = document.getElementsByClassName('button-chose')[0];
+// var activeButton = document.getElementsByClassName('button-chose')[1];
+// var completeButton = document.getElementsByClassName('button-chose')[2];
+var footer = document.getElementsByClassName('footer')[0];
 // AC1:通过在输入框中输入待办事项，按回车键或者「Add」按钮添加一条待办事项；
 // AC2：对于刚添加的待办事项出现在列表中，包含三个部分：项目符号、内容
 // AC3：当什么都没输入时，按回车键或者「Add」按钮不会有任何待办事项被添加
-addButton.onclick = function() {
+
+addInput.addEventListener('keydown', function(event) {
+    if (event.keyCode == 13) {
+        addToDoList(event);
+    }
+});
+
+addButton.addEventListener('click', function(event) {
+    addToDoList(event);
+});
+
+toDoList.addEventListener('click', function(event) {
+    var eventTarget = event.target;
+    if ('hasCompleted' === eventTarget.id) {
+        var index = eventTarget.parentElement.getAttribute("item-index");
+        var toDoThing = JSON.parse(localStorage.getItem(index));
+        toDoThing.hasDone = eventTarget.checked;
+        localStorage.setItem(index, JSON.stringify(toDoThing));
+        console.log(eventTarget.checked);
+        eventTarget.parentElement.firstElementChild.checked = eventTarget.checked;
+        console.log(eventTarget.parentElement.firstElementChild.checked);
+        if (toDoThing.hasDone) {
+            eventTarget.parentNode.style.textDecorationLine = "line-through";
+        } else {
+            eventTarget.parentNode.style.textDecorationLine = "none";
+        }
+    }
+});
+
+
+// AC4：当底部按钮选中「Active」时，输入框中添加 todo，会显示新添加的 todo；
+// AC5：当底部按钮选中「Complete」时，输入框中添加 todo，不会显示新添加的 todo；
+
+// console.log(toDoThings)
+footer.addEventListener('click', function(event) {
+    var eventTarget = event.target;
+    switch (eventTarget.innerHTML) {
+        case 'ALL':
+            // for (var i = 0, len = toDoThings.length; i < len; i++) {
+            //     console.log(toDoThings[i].firstElementChild.checked);
+            //     toDoThings[i].style.visibility = toDoThings[i].firstElementChild.checked ? "" : "";
+            // };
+            for (var li of toDoThings) {
+                li.hidden = false
+            }
+            break;
+        case 'Active':
+            // for (var i = 0, len = toDoThings.length; i < len; i++) {
+            //     console.log(toDoThings[i].firstElementChild.checked);
+            //     toDoThings[i].style.visibility = toDoThings[i].firstElementChild.checked ? "none" : "";
+            // };
+            for (var li of toDoThings) {
+                li.hidden = li.firstElementChild.checked;
+            }
+            break;
+        case 'Complete':
+            for (var li of toDoThings) {
+                li.hidden = !li.firstElementChild.checked;
+            }
+            break;
+    }
+});
+
+// activeButton.addEventListener('click', function(event) {
+//     choseActiveThing(event);
+// });
+
+
+function addToDoList(event) {
     if (addInput.value) {
         var storage = window.localStorage;
         var index = storage.length;
@@ -20,11 +94,13 @@ addButton.onclick = function() {
 }
 
 function addToDoThing(toDoThing) {
-    toDoList.innerHTML = `<li><input type="checkbox"/><span>${toDoThing.content}</span></li>`;
+    toDoList.innerHTML += `<li item-index="${toDoThing.index}"><input id="hasCompleted" type="checkbox" check=${toDoThing.hasDone} /><span>${toDoThing.content}</span></li>`;
 }
-// AC4：当底部按钮选中「Active」时，输入框中添加 todo，会显示新添加的 todo；
-// AC5：当底部按钮选中「Complete」时，输入框中添加 todo，不会显示新添加的 todo；
 
+// function choseActiveThing(event) {
+
+
+// }
 
 // AC1：当点击某条待办事项，代表完成该事项，点击后该事项前面的项目符号改变，文字变灰色并出现删除线；
 
