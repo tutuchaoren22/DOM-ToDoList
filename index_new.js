0aca2542cbfc88b0b83a02ee9fc0820bc582c690
duyuqian <15738381799@163.com>
2019-12-26 09:18:16 +0800

    [duyuqian] fix: Add effect display gif file

--- index_new.js
+++ index_new.js
@@ -6,11 +6,42 @@ var footer = document.getElementsByClassName('footer')[0];
 var buttonChose = document.getElementsByClassName("button-chose");
 var currentButton = "ALL";
 
+
+function getlocalStorage() {
+    var storage = window.localStorage;
+    var localStorageKeys = getLocalStorageKeys();
+    if (storage.length) {
+        localStorageKeys.forEach(function(value, i) {　　
+            var obj = JSON.parse(storage.getItem(value));
+            var toDoThing = {
+                "index": Object.values(obj)[0],
+                "content": Object.values(obj)[1],
+                "hasDone": Object.values(obj)[2]
+            };
+            addToDoThing(toDoThing);
+        })
+    }
+}
+
+function getLocalStorageKeys() {
+    keyArr = [];
+    for (var item in localStorage) {
+        if (localStorage.hasOwnProperty(item)) {
+            keyArr.push(item);
+        }
+    }
+    keyArr.sort(function(a, b) {
+        return a - b;
+    });
+    return keyArr
+}
+
 //将input内容添加到list
 function addToDoList() {
+    var localStorageKeys = getLocalStorageKeys();
     if (addInput.value) {
         var storage = window.localStorage;
-        var index = storage.length;
+        var index = (0 == storage.length) ? 0 : parseInt(localStorageKeys.slice(-1)) + 1;
         var toDoThing = {
             "index": index,
             "content": addInput.value,
@@ -29,27 +60,12 @@ function keyEnterAddToDoList(event) {
     }
 }
 //给ol添加list
-// function addToDoThing(toDoThing) {
-//     console.log(toDoThing.hasDone);
-//     toDoList.innerHTML += `<li item-index="${toDoThing.index}">
-//   <input class="hasCompleted" type="checkbox" />
-//   <span>${toDoThing.content}</span>
-//   <button class="button-delete">×</button>
-//   </li>`; //checked="${toDoThing.hasDone}"
-// }
-
 function addToDoThing(toDoThing) {
     var li = document.createElement("li");
     li.setAttribute("item-index", toDoThing.index);
-    var checkBox = document.createElement("input");
-    setAttributes.call(checkBox, ["type", "class"], ["checkbox", "hasCompleted"]);
-    checkBox.checked = toDoThing.hasDone;
-    var content = document.createElement("span");
-    content.innerHTML = toDoThing.content;
-    var deleteButton = document.createElement("button");
-    deleteButton.innerHTML = "×";
-    setAttributes.call(deleteButton, ["class"], ["button-delete"]);
-    appendChildren.call(li, [checkBox, content, deleteButton]);
+    li.innerHTML = `<input class="hasCompleted" type="checkbox" />
+      <span>${toDoThing.content}</span>
+      <button class="button-delete">×</button>`;
     toDoList.appendChild(li);
 }
 
@@ -71,12 +87,16 @@ function hasCompleted(eventTarget) {
     var toDoThing = JSON.parse(localStorage.getItem(index));
     toDoThing.hasDone = eventTarget.checked;
     localStorage.setItem(index, JSON.stringify(toDoThing));
-    if (eventTarget.checked) {
-        eventTarget.parentNode.style.textDecorationLine = "line-through";
-        eventTarget.parentNode.style.color = "lightgray";
+    checkboxStyle(eventTarget);
+}
+
+function checkboxStyle(e) {
+    if (e.checked) {
+        e.parentNode.style.textDecorationLine = "line-through";
+        e.parentNode.style.color = "lightgray";
     } else {
-        eventTarget.parentNode.style.textDecorationLine = "";
-        eventTarget.parentNode.style.color = "black";
+        e.parentNode.style.textDecorationLine = "none";
+        e.parentNode.style.color = "black";
     }
 }
 //每条li后面的删除符号的点击事件；
@@ -93,11 +113,11 @@ function toDoListEvent(event) {
     switch (eventTarget.className) {
         case 'hasCompleted':
             hasCompleted(eventTarget);
-            updateButtonStatus();
+            // updateButtonStatus();
             break;
         case 'button-delete':
             buttonDelete(eventTarget);
-            updateButtonStatus();
+            // updateButtonStatus();
             break;
         default:
             break;
@@ -151,6 +171,7 @@ function footerEvent(event) {
     updateButtonStyle();
 }
 
+getlocalStorage();
 //todolist整个ol列表，添加事件监听函数
 toDoList.addEventListener('click', toDoListEvent);
 //给页脚三个按钮添加事件监听函数
